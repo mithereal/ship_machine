4c5e304591e0c52e6ea9088fd4589a3bc3287faa
Jason Clark <mithereal@gmail.com>
2019-03-11 14:38:37 -0700

    .

--- entry.sh
+++ entry.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# start cron
-/usr/sbin/crond -f -l 8
-
 # fetch amazon mws node 
 npm i amazon-mws --save
 npm i mysql --save
 npm i xml-js --save
 
 chmod +x /nodejs/fetch_amazon_orders.js
+
+# start cron
+/usr/sbin/crond -f -l 8
 
